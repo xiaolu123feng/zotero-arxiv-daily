8fca0ecada7b516aab2946691160df16cc0e7a52
xiaolu123feng <139882218+xiaolu123feng@users.noreply.github.com>
2025-07-16 16:14:30 +0800

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -9,8 +9,16 @@ def rerank_paper(candidate:list[ArxivPaper],corpus:list[dict],model:str='avsolat
     corpus = sorted(corpus,key=lambda x: datetime.strptime(x['data']['dateAdded'], '%Y-%m-%dT%H:%M:%SZ'),reverse=True)
     time_decay_weight = 1 / (1 + np.log10(np.arange(len(corpus)) + 1))
     time_decay_weight = time_decay_weight / time_decay_weight.sum()
-    corpus_feature = encoder.encode([paper['data']['abstractNote'] for paper in corpus])
-    candidate_feature = encoder.encode([paper.summary for paper in candidate])
+
+    candidate_texts = [paper.summary for paper in candidate]
+    corpus_texts = [p['data']['abstractNote'] for p in corpus]
+    candidate_feature = encoder.encode(candidate_texts, convert_to_tensor=True)
+    corpus_feature = encoder.encode(corpus_texts, convert_to_tensor=True)
+
+    print("candidate_feature shape:", candidate_feature.shape)
+    print("corpus_feature shape:", corpus_feature.shape)
+
+
     sim = encoder.similarity(candidate_feature,corpus_feature) # [n_candidate, n_corpus]
     scores = (sim * time_decay_weight).sum(axis=1) * 10 # [n_candidate]
     for s,c in zip(scores,candidate):
